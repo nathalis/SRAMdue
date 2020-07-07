5a07a5ad1512bd1e10fd08abf35403fa1b9c04d3
nathalis <52428568+nathalis@users.noreply.github.com>
2020-07-07 15:30:29 +0200

    Update SoftwareSpiMaster.cpp

--- SoftwareSpiMaster.cpp
+++ SoftwareSpiMaster.cpp
@@ -5,6 +5,16 @@
  */
 #include "SoftwareSpiMaster.h"
 
+inline void digitalWriteDirect(int pin, boolean val){
+  if(val) g_APinDescription[pin].pPort -> PIO_SODR = g_APinDescription[pin].ulPin;
+  else    g_APinDescription[pin].pPort -> PIO_CODR = g_APinDescription[pin].ulPin;
+}
+
+inline int digitalReadDirect(int pin){
+  return !!(g_APinDescription[pin].pPort -> PIO_PDSR & g_APinDescription[pin].ulPin);
+}
+
+
 SoftwareSpiMaster::SoftwareSpiMaster()
 {
     pinCLK = 0;
@@ -43,13 +53,10 @@ uint8_t SoftwareSpiMaster::transfer(uint8_t value)
     {
         for(uint8_t i = 0; i < 8; ++i)
         {
-            digitalWrite(pinMOSI, (bool) (value & (1 << (7 - i))));
-            delay(1);
-            digitalWrite(pinCLK, HIGH);
-            delay(1);
-            readValue |= ((uint8_t) digitalRead(pinMISO)) << (7 - i);
-            digitalWrite(pinCLK, LOW);
-            delay(1);
+            digitalWriteDirect(pinMOSI, (bool) (value & (1 << (7 - i))));
+            digitalWriteDirect(pinCLK, HIGH);
+            readValue |= ((uint8_t) digitalReadDirect(pinMISO)) << (7 - i);
+            digitalWriteDirect(pinCLK, LOW);
         }
     }
     return readValue;
